b8dd9faf690b09100e757baabefda4777b1a6453
Sajana Hemandra Weerawardhena <sajana@stanford.edu>
2017-12-20 19:07:56 +0530

    first commit

---
+++ screens/feed.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Text} from 'react-native';
+
+class foodFeed extends Component {
+    render(){
+        <Text>well come to your feed</Text>
+    }
+}
+
+export default foodFeed;
